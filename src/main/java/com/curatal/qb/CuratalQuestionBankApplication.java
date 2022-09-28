2d8865ee235ad186985757aeb16c8e61a4be679d
sreeragsearchtek <sreerag@firstipo.in>
2022-09-28 20:32:19 +0530

    Update CuratalQuestionBankApplication.java

--- src/main/java/com/curatal/qb/CuratalQuestionBankApplication.java
+++ src/main/java/com/curatal/qb/CuratalQuestionBankApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//public class CuratalQuestionBankApplication{
+public class CuratalQuestionBankApplication{
 
 	//Deploy Application in Stand-Alone Tomcat Server
-public class CuratalQuestionBankApplication extends SpringBootServletInitializer {
+//public class CuratalQuestionBankApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CuratalQuestionBankApplication.class, args);
